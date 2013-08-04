426ec55aaa9b5ddaa774fcf49af1229ea0e3ffb3
daw2 <jkdcam83@hotmail.com>
2013-08-04 19:30:58 +0200

    Hago comentario línea 23 (Pep)

--- HelloWorld/src/com/example/helloworld/MainActivity.java
+++ HelloWorld/src/com/example/helloworld/MainActivity.java
@@ -19,6 +19,8 @@ public class MainActivity extends Activity {
 
     //Añado un comentario desde Github como si estubiéramos trabajando varias personas en un mismo proyecto 
     //y alguien sube algo que me interesa bajar a mi repositorio local
+    
+    //a ver si marcha bien desde Pep
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
