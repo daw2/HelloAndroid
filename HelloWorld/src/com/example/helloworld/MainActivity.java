0a7f521020d9712dde0a8371b601088f158670c9
Cristina <jkdcam83@hotmail.com>
2013-08-04 19:18:57 +0200

    commit1 desde makina 2

---
+++ HelloWorld/src/com/example/helloworld/MainActivity.java
@@ -0,0 +1,30 @@
+package com.example.helloworld;
+
+import android.os.Bundle;
+import android.app.Activity;
+import android.view.Menu;
+import android.widget.TextView;
+
+public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+    }
+    
+    // Esto es un comentario 
+
+
+    //Añado un comentario desde Github como si estubiéramos trabajando varias personas en un mismo proyecto 
+    //y alguien sube algo que me interesa bajar a mi repositorio local
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
+    }
+    
+}
